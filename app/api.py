dccea1d26c5e3fa130aed92015370bb662e54cb9
requinard <david@requinard.nl>
2020-10-01 12:16:01 +0200

    add testing for api and drone step

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, File
 from starlette.responses import RedirectResponse
 from typing import List, Any
 
-from models import TransactionCollection
+from models import TransactionCollection, TransactionRecord
 from operations import load_file, parse_to_models
 
 app = FastAPI(
@@ -20,19 +20,19 @@ def redirect_to_docs():
     return RedirectResponse("/docs")
 
 
-@app.post("/transactions/validate")
-def validate_transaction_collection(transactions: List[Any]):
+@app.post("/transactions/validate", response_model=TransactionCollection)
+def validate_transaction_collection(transactions: List[TransactionRecord]):
     """
-    Validate a list of transactions in JSON format.
+    Validate a list of transactions in JSON format. Will automatically check if all reference ID's are unique
     """
     result = parse_to_models(transactions, "application/json")
 
     return result
 
 @app.post("/transactions/validate/single")
-def validate_transaction_single(transaction: Any):
+def validate_transaction_single(transaction: TransactionRecord):
     """
-    Validate a single transaction
+    Validate a single transaction.
     """
     result = parse_to_models([transaction], "application/json")
 
@@ -43,7 +43,7 @@ def validate_transactions_upload(
         file: UploadFile = File(...)
 ) -> TransactionCollection:
     """
-    Validate a list of transactions from a file upload.
+    Validate a list of transactions from a file upload. Will validate reference uniqueness
     """
     if file.content_type not in accepted_content_types:
         raise ValueError("File is not in an accepted content-type")
