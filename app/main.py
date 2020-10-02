e696b31d6c61541846a3b129937c70a41c0aaae4
requinard <david@requinard.nl>
2020-10-02 13:23:58 +0200

    add expressive cli things

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
+import typer
 import uvicorn
 
 from api import app
-from mangum import Mangum
 
-lambda_handler = Mangum(app)
+cli = typer.Typer()
+
+
+@cli.command()
+def run(
+    host: str = typer.Option("127.0.0.1"),
+    port: int = typer.Option(8080),
+    reload: bool = typer.Option(True)
+):
+    uvicorn.run(app, host=host, port=port, reload=reload)
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, )
+    cli()
